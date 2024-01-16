91a8ac241ed7969244b935585449e398c96d0f0b
Patrick Hinners <patrick.hinners@outlook.com>
2024-01-16 07:35:28 +0100

    Started sim2real development

---
+++ camera_librealsense.py
@@ -0,0 +1,100 @@
+import time
+
+import cv2 as cv
+import numpy as np
+import pyrealsense2 as rs
+
+
+class Camera:
+    def __init__(self):
+        # for librealsense
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        # for aruco detection
+        self.arucoDict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
+        self.arucoParams = cv.aruco.DetectorParameters()
+        self.detector = cv.aruco.ArucoDetector(self.arucoDict, self.arucoParams)
+        self.corners = None
+        self.ids = []
+
+    def start(self):
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        self.pipeline.start(self.config)
+        time.sleep(2)
+
+    def get_frame(self):
+        frames = self.pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        color_image = np.asanyarray(color_frame.get_data())
+        return color_image
+
+    def get_distance(self, frame, add_to_frame=True):
+        centers, frame = self.get_centers(frame, add_to_frame=True)
+        # get idx
+        ids = list(self.ids)
+        bottom_stat_idx = ids.index(22)
+        bottom_dyn_idx = ids.index(32)
+        top_stat_idx = ids.index(2)
+        top_dyn_idx = ids.index(12)
+
+        dist_bottom = np.linalg.norm(centers[bottom_stat_idx] - centers[bottom_dyn_idx], ord=2)
+        # dist_bottom = np.abs(centers[bottom_stat_idx][0] - centers[bottom_dyn_idx][0])
+        dist_top = np.linalg.norm(centers[top_stat_idx] - centers[top_dyn_idx], ord=2)
+        # dist_top = np.abs(centers[top_stat_idx][0] - centers[top_dyn_idx][0])
+        dists = [dist_bottom, dist_top]
+        indexs = [bottom_stat_idx, top_stat_idx]
+        real_distances = []
+        for i in range(len(indexs)):
+            corner = self.corners[indexs[i]].reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corner
+            pixel_len = (abs(topLeft[0] - topRight[0]) + abs(bottomLeft[0] - bottomRight[0])) / 2
+            length_per_pixel = 0.024 / pixel_len
+            real_distances.append(np.round(dists[i] * length_per_pixel, 3))
+
+        if add_to_frame:
+            ids = [bottom_stat_idx, bottom_dyn_idx, top_stat_idx, top_dyn_idx]
+            for i in range(len(ids) // 2):
+                cv.line(frame, centers[ids[2 * i]], centers[ids[2 * i + 1]], (255, 0, 0), 2)
+                cv.putText(frame, str(real_distances[i]),
+                           (int((centers[2 * i][0] + centers[2 * i + 1][0]) / 2), centers[2 * i][1] - 15),
+                           cv.FONT_HERSHEY_SIMPLEX, 0.5,
+                           (0, 0, 255), 2)
+        return np.array(real_distances), frame
+
+    def get_centers(self, frame, add_to_frame=True):
+        (self.corners, self.ids, rejected) = self.detector.detectMarkers(frame)
+        centers = []
+        self.ids = self.ids.flatten()
+        # assert len(self.ids) >= 4, "Not all Arucos detected!!!"
+        for i in range(len(self.corners)):
+            corner = self.corners[i].reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corner
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            if add_to_frame:
+                # draw the bounding box of the ArUCo detection
+                cv.line(frame, topLeft, topRight, (0, 255, 0), 2)
+                cv.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+                cv.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+
+            # ArUco marker
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            if add_to_frame:
+                cv.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+            centers.append([cX, cY])
+            # centers[self.ids[i]] = np.array([cX, cY])
+        return np.array(centers), frame
+
+    def stop(self):
+        self.pipeline.stop()
+
+
+if __name__ == "__main__":
+    camera = Camera()
+    camera.start()
---
+++ play_real.py
@@ -0,0 +1,206 @@
+import time
+
+import numpy as np
+
+from camera_librealsense import Camera
+from common_real import get_args
+from env_ext_real import make_env
+from envs_real import register_custom_envs
+from franka_robot import FrankaRobot
+from mpc_real import MPCDebugPlot
+from policies_real import Policy, make_policy
+
+
+class Player:
+    policy: Policy = None
+    debug_plot: MPCDebugPlot = None
+    # debug_plot: MPCDebugPlotSmall = None
+    mpc_policy = False
+
+    obst_sizes = {"FrankaPickDynSqrObstacles-v1": np.array([[0.015, 0.017, 0.015], [0.015, 0.017, 0.015]]),
+                  "FrankaPickDynObstacles-v1": np.array([[0.045, 0.017, 0.015], [0.015, 0.017, 0.015]]),
+                  "FrankaPickDynObstacles-v2": np.array([[0.045, 0.017, 0.015], [0.015, 0.017, 0.015]]),
+                  "FrankaPickDynLiftedObstacles-v1": np.array(
+                      [[0.045, 0.017, 0.02], [0.015, 0.017, 0.02], [0.24, 0.03, 0.025]])}
+
+    obst_vels = {"FrankaPickDynSqrObstacles-v1": np.array([0.02, 0.03]),
+                 "FrankaPickDynObstacles-v1": np.array([0.0, 0.03]),
+                 "FrankaPickDynObstacles-v2": np.array([0.02, 0.03]),
+                 "FrankaPickDynLiftedObstacles-v1": np.array([0.02, 0.03, 0.0])}
+
+    def __init__(self, args):
+        # mujoco env set
+        self.env = make_env(args)
+        # policy set
+        self.policy = make_policy(args)
+        self.policy.set_env(env=self.env)
+        # robot set
+        self.robot = FrankaRobot("192.168.5.12")
+        self.gripper = self.robot.gripper
+        self.block_z = False if args.env == 'FrankaPickDynLiftedObstacles-v1' else True
+        # real env set
+        self.offset = np.array([0.8, 0.75, 0.4])  # robot base relative to the origin in simulator
+        self.goal = np.array([0.5 - 0.07, -0.3, 0.0]) + self.offset
+        # self.subgoal = self.goal
+        if self.block_z:
+            self.init = np.array([0.5, 0.3, 0.065 + 0.09])  # for FrankaPickDynLiftedObstacles-v1
+            self.obst_rel_robot = np.array([[0.5, 0.1, 0.02], [0.5, -0.1, 0.05 + 0.02],
+                                            [0.5, -0.1, 0.025]])  # middle pose relative to robot base
+            self.z_offset = 0.00 - 0.065 - 0.09
+        else:
+            self.init = np.array([0.5, 0.3, 0.15])
+            self.obst_rel_robot = np.array([[0.5, 0.1, 0.15], [0.5, -0.1, 0.15]])  # middle pose relative to robot base
+
+        self.pre_dists = np.array([None, None])
+        self.signs = np.array([1, 1])
+        self.dt = 0.5  # time interval in real env
+        self.length = 80  # number of steps to take
+        self.obst_size = self.obst_sizes[args.env]  # (x/2, y/2)
+        self.vels = self.obst_vels[args.env]  # velocity of obstacle
+        self.pos_dif = 0.1
+        self.center_x = 0.5 + self.offset[0]
+        self.origin_offset = np.array([0.042, 0.038])
+
+        # camera set
+        self.camera = Camera()
+
+    def initialize(self):
+        # start camera
+        self.camera.start()
+        # move robot to initial pose
+        self.robot.move([0, 0, 0.2])
+        pose = self.robot.current_pose()[:3]
+        pose[:2] = self.init[:2]
+        self.robot.move_to_init(pose)
+        self.gripper.move(0.05)
+        input("Enter to lower the gripper.")
+        if self.block_z:
+            pose[2] = 0.065
+        else:
+            pose[2] = 0.0065
+        self.robot.move_to_init(pose)
+        input("Enter to grasp the object.")
+        self.robot.clamp()
+        self.robot.move_to_init(self.init)
+        input("Enter to start moving.")
+
+    def get_obs_distance(self):
+        frame = self.camera.get_frame()
+        try:
+            dists, _ = self.camera.get_distance(frame, add_to_frame=False)
+        except:
+            dists = self.origin_offset
+        dists -= self.origin_offset  # relative to origin
+        return dists
+
+    def get_obs_pose(self, dists):
+        # get obstacle pose
+        dyn_obstacles = []
+        for i in range(len(dists)):
+            dyn_obstacle = np.append(self.obst_rel_robot[i] + self.offset, self.obst_size[i])
+            dyn_obstacle[0] += dists[i] - self.pos_dif
+            dyn_obstacles.append(dyn_obstacle)
+        return np.array(dyn_obstacles)
+
+    def finish(self):
+        self.camera.stop()
+        cur_pose = self.robot.current_pose()
+        if self.block_z:
+            disp_z = cur_pose[2] - 0.065
+        else:
+            disp_z = cur_pose[2] - 0.0065
+        self.robot.move([0, 0, -disp_z])
+        self.robot.release()
+
+    def close_to_goal(self, xinit):
+        # cur_pos = self.robot.current_pose()[:2]
+        goal = self.goal
+        if np.linalg.norm(xinit[:2] - goal[:2], 2) <= 0.02:
+            return True
+        return False
+
+    def play(self):
+        xinit = self.init + self.offset
+        dists = self.get_obs_distance()
+        ob = self.env.reset()
+        # print("original obs", ob)
+        ob = self.set_obs(ob, xinit, dists)
+        if args.play_policy in ['MPCPolicy']:
+            self.policy.set_sub_goal(ob['desired_goal'])
+
+        while not self.close_to_goal(xinit):
+            # print("xinit: ", xinit)
+            # print('ob', ob)
+            t1 = time.time()
+            # print("observation", ob["observation"])
+            real_actions = self.policy.predict([ob])
+            real_action = real_actions[0]
+            if args.play_policy in ['RLPolicy']:
+                real_action *= 0.05
+                # for xy motion
+                real_action[2] = 0
+            self.robot.move(real_action[:3])
+            print("action:", real_action)
+
+            # update the original pose
+            cur_pose = self.robot.current_pose()
+            cur_joint_pose = self.robot.current_joint_pose()
+            x_init = cur_pose[0] + self.offset[0]
+            y_init = cur_pose[1] + self.offset[1]
+            if self.block_z:
+                z_init = ob['observation'][2]
+            else:
+                z_init = cur_pose[2] + self.offset[2]
+            xinit = np.array([x_init, y_init, z_init])
+            # print("xinit", xinit)
+            dists = self.get_obs_distance()
+            ob = self.set_obs(ob, xinit, dists)
+            t2 = time.time() - t1
+            print("time:", t2)
+            if self.dt - t2 > 0:
+                time.sleep(self.dt - t2)
+        self.finish()
+
+    def set_obs(self, obs, xinit, dists):
+        obs["observation"][:2] = xinit[:2]
+        obs["observation"][3:5] = xinit[:2]
+
+        dyn_obstacles = self.get_obs_pose(dists)
+
+        obs["observation"][9:11] = dyn_obstacles[0, :2]
+        obs["observation"][15:17] = dyn_obstacles[1, :2]
+        obs["real_obstacle_info"][0, :2] = dyn_obstacles[0, :2]
+        obs["real_obstacle_info"][1, :2] = dyn_obstacles[1, :2]
+
+        if self.pre_dists.any():
+            signs = np.sign(dists - self.pre_dists)
+            self.signs[0] = signs[0] if signs[0] != 0 else -self.signs[0]
+            self.signs[1] = signs[1] if signs[1] != 0 else -self.signs[1]
+        self.pre_dists = dists
+        obs["observation"][21] = self.vels[0] * self.signs[0]
+        obs["observation"][24] = self.vels[1] * self.signs[1]
+        obs["obj_vels"][0, 0] = self.vels[0] * self.signs[0]
+        obs["obj_vels"][1, 0] = self.vels[1] * self.signs[1]
+
+        goal = self.goal
+        obs["desired_goal"][:2] = goal[:2]
+
+        if self.block_z:
+            obs["observation"][2] = xinit[2] + self.z_offset
+            obs["observation"][5] = xinit[2] + self.z_offset
+
+        obs["dt"] = self.dt
+        obs["pos_dif"] = self.pos_dif
+        obs["center_x"] = self.center_x
+
+        return obs
+
+
+if __name__ == "__main__":
+    register_custom_envs()
+    # Call play.py in order to see current policy progress
+    args = get_args()
+    player = Player(args)
+    player.initialize()
+    player.play()
+    # player.record_video(raw_path="/home/ssc/bachelor-thesis/videos/rollouts_{}_{}".format(args.env, args.play_policy))
