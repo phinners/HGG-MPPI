3ab015bcb8ef3a07e88c24db1a8f2ae7d541db85
Patrick Hinners <patrick.hinners@outlook.com>
2024-01-29 08:06:45 +0100

    Updated some helper files

--- franka_robot.py
+++ franka_robot.py
@@ -1,8 +1,8 @@
-from frankx import Affine, LinearRelativeMotion, Robot, LinearMotion, InvalidOperationException
+from frankx import Affine, LinearRelativeMotion, Robot, LinearMotion, InvalidOperationException, JointMotion
 
 
 class FrankaRobot:
-    def __init__(self, id="192.168.5.12"):
+    def __init__(self, id="192.168.178.12"):
         self.robot = Robot(id)
         self.gripper = self.robot.get_gripper()
         self.robot.set_default_behavior()
@@ -15,10 +15,13 @@ class FrankaRobot:
         self.robot.move(LinearMotion(Affine(*initial_pos, 0, 0, 0)))
         # self.robot.move(LinearMotion(Affine(0, 0, -0.03, 0, 0, 0)))
 
+    def move_to_initial_joint(self, initial_q_pos):
+        self.robot.move(JointMotion(initial_q_pos))
+
     def clamp(self):
         self.gripper.clamp()
 
-    def release(self, disp=0.05):
+    def release(self, disp=0.06):
         self.gripper.release(disp)
 
     def move(self, displacement):
@@ -39,8 +42,19 @@ class FrankaRobot:
 
 
 if __name__ == "__main__":
+    initial_joints = [-1.4176983079983476, -1.4002775721633642, 1.7748181197400454, -1.976985159857231,
+                      1.4911890585610361, 1.823851506586428, 0.5601574892451364]
     robot = FrankaRobot()
     cur_pose = robot.current_pose()
+    cur_joint_pose, cur_joint_vel = robot.current_joint_state()
+
+    print(cur_pose)
+    print(cur_joint_pose)
+    print(cur_joint_vel)
+
+    input("Move to Initial Joint Pose")
+    robot.move_to_initial_joint(initial_joints)
+
     cur_joint_pose, cur_joint_vel = robot.current_joint_state()
 
     print(cur_pose)
